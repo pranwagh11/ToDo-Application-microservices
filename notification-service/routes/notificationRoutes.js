e27854ea7fa21782af94df517671101eb760d599
Pranav Waghmare <pranwagh11@gmail.com>
2026-07-04 17:13:24 +0530

    Delete notification-service directory

--- notification-service/routes/notificationRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendEmail } = require('../controllers/notificationController');
-
-router.post('/email', sendEmail);
-
-module.exports = router;
